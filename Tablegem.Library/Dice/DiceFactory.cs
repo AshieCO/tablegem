be2ec47df8a480add266346f488781c36a5b4f09
AshieCO <alexanderashie@gmail.com>
2021-08-28 15:34:44 -0400

    die and dice factory with tests

--- Tablegem.Library/Dice/DiceFactory.cs
+++ Tablegem.Library/Dice/DiceFactory.cs
@@ -1,17 +1,57 @@
-﻿using System;
+﻿
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tablegem.Library.Dice.Interfaces;
+using Tablegem.Library.Services.Interfaces;
 
 namespace Tablegem.Library.Dice
 {
     public class DiceFactory : IDiceFactory
     {
+        private IConfiguration _config;
+        private IRandomNumberGenerator _randomNumberGenerator;
+        private bool _useValidPolyhedralDice;
+        private List<int> _validPolyhedralDiceFaceNumbers;
+
+        public DiceFactory(IConfiguration config, IRandomNumberGenerator randomNumberGenerator)
+        {
+            _config = config;
+            _randomNumberGenerator = randomNumberGenerator;
+            _useValidPolyhedralDice = _config.GetSection("UseValidPolyhedralDice").Get<bool>();
+            _validPolyhedralDiceFaceNumbers = _config.GetSection("ValidPolyhedralDiceFaceNumbers").Get<List<int>>();
+        }
+
         public Die CreateDie(int numberOfFaces)
         {
-            throw new NotImplementedException("also implement configuration and/or validation?");
+            if (InputIsValid(numberOfFaces))
+            {
+                return new Die(_randomNumberGenerator, numberOfFaces);
+            }
+            else
+            {
+                throw new ArgumentException($"The number of faces '{numberOfFaces}' is invalid for creating a new Die.");
+            }
+        }
+
+        private bool InputIsValid(int numberOfFaces)
+        {
+            if (!_useValidPolyhedralDice)
+            {
+                return true;
+            }
+            else
+            {
+                if (_validPolyhedralDiceFaceNumbers.Contains(numberOfFaces))
+                {
+                    return true;
+                }
+
+                return false;
+            }
         }
     }
 }
---
+++ Tablegem.UnitTests/DiceFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tablegem.Library.Dice;
+using Tablegem.Library.Services;
+using Xunit;
+
+namespace Tablegem.UnitTests
+{
+    public class DiceFactoryTests
+    {
+        [Fact]
+        public void DiceFactory_ThrowsArgumentException_WhenAttemptingInvalidDieCreation()
+        {
+            var appSettingsJson = "{ \"UseValidPolyhedralDice\": true, \"ValidPolyhedralDiceFaceNumbers\": [ 4, 6, 8, 10, 12, 20, 100 ] }";
+            var configuration = MockConfiguration(appSettingsJson);
+            var randomNumberGenerator = new RandomNumberGenerator();
+
+            var diceFactory = new DiceFactory(configuration, randomNumberGenerator);
+
+            Assert.Throws<ArgumentException>(() => diceFactory.CreateDie(5));
+        }
+
+        [Fact]
+        public void DiceFactory_CreatesDie_WhenAttemptingValidDieCreation()
+        {
+            var appSettingsJson = "{ \"UseValidPolyhedralDice\": true, \"ValidPolyhedralDiceFaceNumbers\": [ 4, 6, 8, 10, 12, 20, 100 ] }";
+            var configuration = MockConfiguration(appSettingsJson);
+            var randomNumberGenerator = new RandomNumberGenerator();
+
+            var diceFactory = new DiceFactory(configuration, randomNumberGenerator);
+
+            Assert.NotNull(diceFactory.CreateDie(6));
+        }
+
+        [Fact]
+        public void DiceFactory_DoesNotThrowArgumentException_WhenValidDiceFalseWhileAttemptingOddDieCreation()
+        {
+            var appSettingsJson = "{ \"UseValidPolyhedralDice\": false, \"ValidPolyhedralDiceFaceNumbers\": [ 4, 6, 8, 10, 12, 20, 100 ] }";
+            var configuration = MockConfiguration(appSettingsJson);
+            var randomNumberGenerator = new RandomNumberGenerator();
+
+            var diceFactory = new DiceFactory(configuration, randomNumberGenerator);
+
+            Assert.NotNull(diceFactory.CreateDie(7));
+        }
+
+        private IConfiguration MockConfiguration(string mockJsonFile)
+        {
+            return new ConfigurationBuilder().AddJsonStream(new MemoryStream(Encoding.ASCII.GetBytes(mockJsonFile))).Build();
+        }
+    }
+}
